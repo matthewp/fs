c7663689af42b7480919779d33ad2a052b74bd9e
Matthew <matthew@phillipsoft.biz>
2013-01-15 09:33:32 -0500

    IndexedDB implementation has basic support for directories.
    Only the most very basic support, still doesn't behave as you would want/expect. Need to use a directory parsing library to get it to same directories correctly so that you can look them up either by ./foo, foo, or foo/  Currently only foo/ will work.

--- indexeddb.js
+++ indexeddb.js
@@ -1,6 +1,6 @@
 var DB_NAME = window.location.host + '_filesystem',
-    OS_NAME = 'files';
-
+    OS_NAME = 'files',
+    DIR_IDX = 'dir';
 function init(callback) {
   var req = window.indexedDB.open(DB_NAME, 1);
 
@@ -8,7 +8,7 @@ function init(callback) {
     var db = e.target.result;
 
     var objectStore = db.createObjectStore(OS_NAME, { keyPath: 'path' });
-    objectStore.createIndex('dir', 'dir', { unique: false });
+    objectStore.createIndex(DIR_IDX, 'dir', { unique: false });
   };
 
   req.onsuccess = function (e) {
@@ -46,7 +46,7 @@ exports.readFile = function (fileName, callback) {
 };
 
 var dirRegExp = /[\/]{1}([^\/]+[\/])*([^\/]*)/;
-exports.writeFile = function (fileName, data, callback) {
+var writeObject = function (name, type, data, callback) {
   initOS('readwrite', function (os) {
     var dir = fileName.match(dirRegExp);
 
@@ -66,12 +66,58 @@ exports.writeFile = function (fileName, data, callback) {
   });
 };
 
+exports.writeFile = function (fileName, data, callback) {
+  initOS('readwrite', function (os) {
+    var dir = fileName.match(dirRegExp);
+    if (dir) {
+      console.log(dir[1]);
+    }
+
+    var req = os.put({
+      "path": fileName,
+      "dir": (dir && dir[1]) || '/',
+      "data": data
+    });
+
+    req.onerror = function (e) {
+      callback(e);
+    };
+
+    req.onsuccess = function (e) {
+      callback(null);
+    };
+  });
+};
+
 exports.removeFile = function (fileName, callback) {
   initOS('readwrite', function (os) {
     var req = os.delete(fileName);
 
     req.onerror = req.onsuccess = function (e) {
       callback();
+    };
+  });
+};
+
+exports.readdir = function (directoryName, callback) {
+  initOS('readonly', function (os) {
+    var idx = os.index(DIR_IDX);
+    var range = IDBKeyRange.only(directoryName);
+    var req = idx.openCursor(range);
+
+    req.onerror = function (e) {
+      callback(e);
+    };
+
+    var results = [];
+    req.onsuccess = function (e) {
+      var cursor = e.target.result;
+      if (cursor) {
+        results.push(cursor.value);
+        cursor.continue();
+      } else {
+        callback(null, results);
+      }
     };
   });
 };
--- test/test.dir.js
+++ test/test.dir.js
@@ -25,3 +25,35 @@ describe('Empty directory', function () {
   });
 
 });
+
+describe('Directory with files', function () {
+  var fs = require('fs');
+
+  var err, files;
+  before(function (done) {
+    fs.writeFile('./foo/dir-file-one.txt', 'Foo', function () {
+      fs.writeFile('./foo/dir-file-two.txt', 'bar', function () {
+        fs.readdir('foo/', function (e, f) {
+          err = e;
+          files = f;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Listing a directory.', function () {
+    it('The error should be null', function () {
+      assert(err === null);
+    });
+
+    it('Should have an array for the files', function () {
+      assert(Array.isArray(files));
+    });
+
+    it('Should have 2 files in the directory.', function () {
+      assert(files.length === 2);
+    });
+  });
+
+});
